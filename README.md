43fc692688bb1239a5f05b22ce2ba89d4ab52c2d
Matthew Scharley <matt.scharley@gmail.com>
2016-12-02 20:14:28 +1100

    Boot-based hello world

---
+++ README.md
@@ -0,0 +1,9 @@
+# parallelism-calculator
+
+Try here: https://mscharley.github.io/parallelism-calculator
+
+A Clojure library designed to ... well, that part is up to you.
+
+## Usage
+
+FIXME
---
+++ src/cljs/parallelism-calculator/core.cljs
@@ -0,0 +1,8 @@
+;; create the main project namespace
+(ns parallelism-calculator.core)
+
+;; enable cljs to print to the JS console of the browser
+(enable-console-print!)
+
+;; print to the console
+(println "Hello, World!")
