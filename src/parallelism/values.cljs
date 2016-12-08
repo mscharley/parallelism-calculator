666dcb718f23de8bed201b40a0f547eb9985f908
Matthew Scharley <matt.scharley@gmail.com>
2016-12-08 22:18:49 +1100

    [GH-9] Add a new record type to help us pass around values/errors
    Closes GH-9

---
+++ src/parallelism/values.cljs
@@ -0,0 +1,17 @@
+(ns parallelism.values
+  "Records and such that we use for passing around values and errors.")
+
+(defrecord Value [valid value message])
+
+(defn value [value]
+  "Create a new value object."
+  (->Value true value nil))
+
+(defn error [value message]
+  "Create a new error object."
+  (->Value false value message))
+
+(defn safe-value [value]
+  (if (:valid value)
+    (:value value)
+    ""))
