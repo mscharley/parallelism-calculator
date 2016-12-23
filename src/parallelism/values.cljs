c311704d095e5f6a074dcdb202a496bc1017045f
Matthew Scharley <matt.scharley@gmail.com>
2016-12-23 18:23:23 +1100

    First pass at some attempted fixes - leads to browser lockups on load.

--- src/parallelism/values.cljs
+++ src/parallelism/values.cljs
@@ -11,7 +11,8 @@
   "Create a new error object."
   (->Value false value message))
 
-(defn safe-value [value default-value]
-  (if (:valid value)
-    (:value value)
-    default-value))
+(defmulti safe-value :valid)
+(defmethod safe-value true [v]
+  (list (:value v)))
+(defmethod safe-value false [v]
+  ())
