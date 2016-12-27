c7a105f3be6bdb8977ea6cdcc0f205c67487c4a8
Matthew Scharley <matt.scharley@gmail.com>
2016-12-27 16:45:43 +1100

    Fix issues with updates failing.

--- src/parallelism/params.cljs
+++ src/parallelism/params.cljs
@@ -4,19 +4,11 @@
             [javelin.core :as j]
             [parallelism.values :as v]))
 
-(defrecord Param [initial cell])
-
 (defn param [initial]
   "Create a new URL parameter"
-  (->Param initial (j/cell (v/value initial))))
-
-(defn cell-for [param]
-  "Retrieve the cell that represents the parameter."
-  (:cell param))
+  (j/cell (v/value initial)))
 
 (defn route [& params]
   "Generate a routing structure based on a given list of parameters."
-  (let [route (h/route-cell)
-        cells (map cell-for params)
-        values (j/cell= (map deref cells))]
-    values))
+  (let [route (h/route-cell)]
+    (j/cell= params)))
